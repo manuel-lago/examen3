d0d58daf042956eea614c59d961878a1b34ab471
persona <persona@10.0.2.15>
2024-05-22 17:46:07 +0200

    Refactorización del código

---
+++ src/main/java/examen3refactorizado/CCuenta.java
@@ -0,0 +1,100 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package examen3refactorizado;
+
+/**
+ *
+ * @author persona
+ */
+public class CCuenta {
+
+    /**
+     * @return the dSaldo
+     */
+    public double getdSaldo() {
+        return dSaldo;
+    }
+
+    /**
+     * @param dSaldo the dSaldo to set
+     */
+    public void setdSaldo(double dSaldo) {
+        this.dSaldo = dSaldo;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    // Propiedades de la Clase Cuenta
+    private double dSaldo;
+    private String nombre;
+
+    public static void main(String[] args) {
+
+        operativa_cuenta(100);
+    }
+
+    private static void operativa_cuenta(float cantidad) {
+        CCuenta cuenta1 = new CCuenta();
+        
+        System.out.println("Saldo Incial: " + cuenta1.getdSaldo() + " euros");
+
+        cuenta1.ingresar(-100);
+        System.out.println("Saldo Incial: " + cuenta1.getdSaldo() + " euros");
+        cuenta1.ingresar(cantidad);
+        System.out.println("Saldo tras ingreso: " + cuenta1.getdSaldo() + " euros");
+        cuenta1.ingresar(2000);
+        System.out.println("Saldo tras ingreso: " + cuenta1.getdSaldo() + " euros");
+
+        cuenta1.ingresar(300);
+        System.out.println("Saldo tras ingreso: " + cuenta1.getdSaldo() + " euros");
+        cuenta1.retirar(50);
+        System.out.println("Saldo tras retirada: " + cuenta1.getdSaldo() + " euros");
+    }
+
+    /* Nuevo comentario para el commit, metodo para ingresar cantidades en la cuenta. Modifica el saldo. */
+    public int ingresar(double cantidad) {
+        int iCodErr;
+
+        if (!(cantidad >= 0 || cantidad <= 3000)) {
+            System.out.println("No se puede ingresar una cantidad negativa ni superior a 3000€ (sin ser notificada con formulario)");
+            iCodErr = 1;
+        } else if (cantidad == 3000) {
+            System.out.println(
+                    "Le recordamos que estamos obligados a notificar a Hacienda dicho ingreso (superior o igual a 3000€)");
+            setdSaldo(getdSaldo() + cantidad);
+            iCodErr = 0;
+        } else {
+            setdSaldo(getdSaldo() + cantidad);
+            iCodErr = 0;
+        }
+
+        return iCodErr;
+    }
+
+    /*
+     * Metodo para retirar cantidades en la cuenta. Modifica el saldo.
+     */
+    public void retirar(double cantidad) {
+        if (cantidad <= 0) {
+            System.out.println("No se puede retirar una cantidad negativa");
+        } else if (getdSaldo() < cantidad) {
+            System.out.println("No  hay suficiente saldo");
+        } else {
+            setdSaldo(getdSaldo() - cantidad);
+            System.out.println("Le quedan " + getdSaldo() + " en cuenta");
+        }
+    }
+}
